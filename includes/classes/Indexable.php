f6865e19cc16e9311615bfee8e5005e56d8df074
Pau Argelaguet <pau@outlook.com>
2021-07-22 16:45:34 +0200

    Fixing invalid datetime warning

--- includes/classes/Indexable.php
+++ includes/classes/Indexable.php
@@ -487,22 +487,24 @@ abstract class Indexable {
 		$meta_types['datetime'] = '1970-01-01 00:00:01';
 		$meta_types['time']     = '00:00:01';
 
-		try {
-			// is this is a recognizable date format?
-			$new_date  = new \DateTime( $meta_value, \wp_timezone() );
-			$timestamp = $new_date->getTimestamp();
+		if ( false !== strtotime( $meta_value ) ) {
+			try {
+				// is this is a recognizable date format?
+				$new_date  = new \DateTime( $meta_value, \wp_timezone() );
+				$timestamp = $new_date->getTimestamp();
 
-			// PHP allows DateTime to build dates with the non-existing year 0000, and this causes
-			// issues when integrating into stricter systems. This is by design:
-			// https://bugs.php.net/bug.php?id=60288
-			if ( false !== $timestamp && '0000' !== $new_date->format( 'Y' ) ) {
-				$meta_types['date']     = $new_date->format( 'Y-m-d' );
-				$meta_types['datetime'] = $new_date->format( 'Y-m-d H:i:s' );
-				$meta_types['time']     = $new_date->format( 'H:i:s' );
+				// PHP allows DateTime to build dates with the non-existing year 0000, and this causes
+				// issues when integrating into stricter systems. This is by design:
+				// https://bugs.php.net/bug.php?id=60288
+				if ( false !== $timestamp && '0000' !== $new_date->format( 'Y' ) ) {
+					$meta_types['date']     = $new_date->format( 'Y-m-d' );
+					$meta_types['datetime'] = $new_date->format( 'Y-m-d H:i:s' );
+					$meta_types['time']     = $new_date->format( 'H:i:s' );
+				}
+			} catch ( \Exception $e ) {
+				// if $meta_value is not a recognizable date format, DateTime will throw an exception,
+				// just catch it and move on.
 			}
-		} catch ( \Exception $e ) {
-			// if $meta_value is not a recognizable date format, DateTime will throw an exception,
-			// just catch it and move on.
 		}
 
 		return $meta_types;

fd715f9e2150506c9e2e18dcd631763ce8137369
Rebecca Hum <rebeccahum@gmail.com>
2024-08-15 13:50:46 -0600

    Add skip_query_integration from upstream commit

--- tests/php/indexables/TestPost.php
+++ tests/php/indexables/TestPost.php
@@ -7515,4 +7515,38 @@ class TestPost extends BaseTestCase {
 		$this->assertTrue( $query->elasticsearch_success );
 	}
 
+	/**
+	 * Test that query with unsupported orderby does not use EP.
+	 *
+	 * @since 4.5.0
+	 */
+	public function testQueryWithUnSupportedOrderByDoesNotUseEP() {
+		// test for post__in
+		$query = new \WP_Query( array(
+			'orderby' => 'post__in',
+			'ep_integrate' => true,
+		) );
+		$this->assertNull( $query->elasticsearch_success  );
+
+		// test for post_name__in
+		$query = new \WP_Query( array(
+			'orderby' => 'post_name__in',
+			'ep_integrate' => true,
+		) );
+		$this->assertNull( $query->elasticsearch_success  );
+
+		// test for post_parent__in
+		$query = new \WP_Query( array(
+			'orderby' => 'post_parent__in',
+			'ep_integrate' => true,
+		) );
+		$this->assertNull(  $query->elasticsearch_success  );
+
+		// test for parent
+		$query = new \WP_Query( array(
+			'orderby' => 'parent',
+			'ep_integrate' => true,
+		) );
+		$this->assertNull(  $query->elasticsearch_success  );
+	}
 }
